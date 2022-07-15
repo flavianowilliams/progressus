b08c63603b28064e99826417d7a13e72bead48e5
Flaviano Williams Fernandes <44038103+flavianowilliams@users.noreply.github.com>
2022-07-14 22:02:33 -0300

    update deploy.py

--- progressus/settings/deploy.py
+++ progressus/settings/deploy.py
@@ -2,4 +2,6 @@ import os
 
 from progressus.settings.settings import *
 
-os.environ['DEBUG'] = 'True'
+os.environ['DEBUG'] = 'False'
+
+os.environ['ALLOWED_HOSTS'] = ['200.17.101.198', '127.0.0.1', '10.10.0.30']

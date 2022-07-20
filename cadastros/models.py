814e8ce3a66779fb87ff75669cdf50bfba850f30
Flaviano Williams Fernandes <44038103+flavianowilliams@users.noreply.github.com>
2022-07-20 16:45:23 -0300

    adicionando modelo Chamada e CadastroChamada

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,3 +1,4 @@
+from chamadas.models import Chamada
 from users.models import Profile
 from django.db import models
 
@@ -5,5 +6,10 @@ from django.db import models
 
 class CadastroProfile(models.Model):
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+class CadastroChamada(models.Model):
+    chamada = models.OneToOneField(Chamada, on_delete=models.CASCADE)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
--- chamadas/models.py
+++ chamadas/models.py
@@ -1,3 +1,46 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+
+class Chamada(models.Model):
+    nome = models.CharField(max_length=7, unique=True)
+    deadline_inscricao = models.DateField(verbose_name='Data limite da inscrição', null=True)
+    deadline_bibliografia = models.DateField(verbose_name='Data limite da bibliografia', null=True)
+    deadline_proposta = models.DateField(verbose_name='Data limite da proposta', null=True)
+    deadline_projeto = models.DateField(verbose_name='Data limite do projeto', null=True)
+    deadline_chamada = models.DateField(verbose_name='Data limite da chamada', null=True)
+    resumo = models.TextField()
+    edital = models.FileField()
+    aviso = models.TextField(null=True, blank=True)
+
+    def get_status(self):
+        if date.today() > self.deadline_inscricao:
+            if date.today() > self.deadline_chamada:
+                data = "Encerrado"
+            else:
+                data = "Em andamento"
+        else:
+            data = "Aberto"
+        return data
+
+    def __str__(self):
+        return self.nome
+
+    def get_bibliografia(self):
+        if date.today() > self.deadline_bibliografia:
+            return False
+        else:
+            return True
+
+    def get_proposta(self):
+        if date.today() > self.deadline_proposta:
+            return False
+        else:
+            return True
+
+    def get_projeto(self):
+        if date.today() > self.deadline_projeto:
+            return False
+        else:
+            return True
